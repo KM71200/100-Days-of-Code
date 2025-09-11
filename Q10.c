3441a0258015f74d75e0520467cd0541bcf69d48
Karmanya Maletha <karmanyamaletha@gmail.com>
2025-09-11 21:44:46 +0530

    Q10.c

--- Q10.c
+++ Q10.c
@@ -1,18 +1,5 @@
 //Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
 
-/*
-Sample Test Cases:
-Input 1:
-3661
-Output 1:
-1:1:1
-
-Input 2:
-7322
-Output 2:
-2:2:2
-
-*/
 #include <stdio.h>
 int main()
 {
@@ -27,5 +14,6 @@ int main()
 
     printf("Time in H:M:S format is-  %d:%d:%d", h,m,s);
     return 0;
+
 
 }
