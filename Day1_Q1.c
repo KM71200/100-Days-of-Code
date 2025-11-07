d62703d4090c03435f582456d3934057bfd19510
Karmanya Maletha <karmanyamaletha@gmail.com>
2025-11-07 23:26:35 +0530

    File Rename
    Added sample test cases for input and output.

--- Day1_Q1.c
+++ Day1_Q1.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
-
 //Q1: Write a program to input two numbers and display their sum.
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
 
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+*/
+
+#include <stdio.h>
 int main(){
     int a,b;
     printf("Enter first no:");
@@ -11,4 +22,5 @@ int main(){
     printf("Sum of two no is:%d",a+b);
     return 0;    
 }
+
 
