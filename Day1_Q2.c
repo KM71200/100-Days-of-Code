05e807d27e16ccbe663f94c83656e29e5cd94a6c
Karmanya Maletha <karmanyamaletha@gmail.com>
2025-11-09 15:05:29 +0530

    Update and rename Q2.c to Day1_Q2.c

--- Day1_Q2.c
+++ Day1_Q2.c
@@ -1,10 +1,7 @@
-#include<stdio.h>
-
 //Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
-
+#include<stdio.h>
 int main(){
     int a,b,sum,diff,prod,quot;
-    //int s,d,p,q;
     printf("Enter first number:");
     scanf("%d",&a);
     printf("Enter second no");
@@ -13,6 +10,6 @@ int main(){
     diff=a-b;
     prod=a*b;
     quot=a/b;
-    printf("Sum:%d \nDifference:%d \nProduct:%d \nQuotient:%d",sum,diff,prod,quot);
+    printf("Sum:%d \nDifference:%d \nProduct:%d \nQuotient:%d\n",sum,diff,prod,quot);
     return 0;
 }
