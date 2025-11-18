53b54ed804ed1a06c3034a14c0292f86564c5626
Karmanya Maletha <karmanyamaletha@gmail.com>
2025-11-18 23:02:07 +0530

    Update and rename

--- Day7_Q1.c
+++ Day7_Q1.c
@@ -3,20 +3,15 @@
 #include <stdio.h>
 int main()
 {
-    int y;
-    
+    int y; 
     printf("Enter year:");
     scanf("%d",&y);
     
     if ((y%4==0 || y%400==0) && (y%100 != 0)){
         printf("%d is Leap Year",y);
     }
-
     else{
         printf("%d not a Leap Year",y);
     }
-
     return 0;
-
 }
-
