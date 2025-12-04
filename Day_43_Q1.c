664fc491c87fb053d58ed01c1e579763abeeb034
Karmanya Maletha <karmanyamaletha@gmail.com>
2025-12-04 11:38:52 +0530

    Add files via upload

---
+++ Day_43_Q1.c
@@ -0,0 +1,31 @@
+//Q85: Reverse a string.
+
+/*
+Sample Test Cases:
+Input 1:
+abcd
+Output 1:
+dcba
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int len = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    printf("Reversed string: ");
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
